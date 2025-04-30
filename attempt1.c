4153b4b5de7f673ac15d6d80230000113ffccb5c
Suraj Acharya <surajacharya2005@gmail.com>
2025-04-30 22:05:13 +0530

    made junk packets little little and metadata and all (unenecypted metadata)

--- attempt1.c
+++ attempt1.c
@@ -1,14 +1,16 @@
 //Needs:
 //Encryption and decryption functions. text goes in, text goes out. 
-// function to make and operate on text document or dat document
-//signup, read, edit and delete functions
-//function to split up encrypted text with correct prefixes and make packets.
+// function to make and operate on text document or dat document TODO:
+//signup, read, edit and delete functions   TODO:
+//function to split up encrypted text with correct prefixes and make packets. TODO: encrypt the metadata and add a secodn layer of encryption to the whole thing.
 // till shashi gets here, a function to distribute packets among that folder
 //similarly to piece packets back
 
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
+#include <time.h>
 
 void inputstring(char *what, char *input_to_this_string){
     printf("Enter %s:",what);
@@ -25,15 +27,22 @@ int makekey(char *usn, char *pwd, int *keystream_out){
 void makepackets(char *ciphertext, char packets_out[][26]){
     int j=0;
     char temp[26];
+    int counter=3;
+    strcpy(temp,"");
+    sprintf(temp, "%d%c%d", (int)ceil(strlen(ciphertext)/22.0), ciphertext[0], j+1); //metadata that tells: num_of_packets, identifying attribute, packet_num
     for(int i=0;i<strlen(ciphertext);i++){
-        temp[i%25]=ciphertext[i];
-        if(i%25==24){
+        temp[counter++]=ciphertext[i];
+        if(counter==25){
             temp[25]='\0';
             strcpy(packets_out[j++],temp);
+            if (j>98){return;}
+            strcpy(temp,"");
+            sprintf(temp, "%d%c%d", (int)ceil(strlen(ciphertext)/22.0), ciphertext[0], j+1); //metadata that tells: num_of_packets, identifying attribute, packet_num
+            counter=3;
         }
     }
-    if (strlen(ciphertext) % 25 != 0){
-        temp[strlen(temp)]='\0';
+    if (strlen(ciphertext)%22!=0){
+        temp[counter]='\0';
         strcpy(packets_out[j],temp);
     }
 }
@@ -52,6 +61,26 @@ void decrypt(char *ciphertext, char *plaintext_out, int *keystream, int len){
     plaintext_out[strlen(ciphertext)]='\0'; //null_terminating
 }
 
+void makejunk(char packets_out[][26]){
+    char randtext[]="philosophyofeducationisalabelappliedtothestudyofthepurposeprocessnatureandidealsofeducationitcanbeconsideredabranchofbothphilosophyandeducationeducationcanbedefinedastheteachingandlearningofspecificskillsandtheimpartingofknowledgejudgmentandwisdomandissomethingbroaderthanthesocietalinstitutionofeducationweoftenspeakofmanyeducationalistsconsideritaweakandwoollyfieldtoofarremovedfromthepracticalapplicationsoftherealworldtobeusefulbutphilosophersdatingbacktoplatoandtheancientgreekshavegiventheareamuchthoughtandemphasisandthereislittledoubtthattheirworkhashelpedshapethepracticeofeducationoverthemillenniaplatoistheearliestimportanteducationalthinkerandeducationisanessentialelementintherepublichismostimportantworkonphilosophyandpoliticaltheorywrittenaroundbcinitheadvocatessomeratherextrememethodsremovingchildrenfromtheirmotherscareandraisingthemaswardsofthestateanddifferentiatingchildrensuitabletothevariouscastesthehighestreceivingthemosteducationsothattheycouldactasguardiansofthecityandcareforthelessablehebelievedthateducationshouldbeholisticincludingfactsskillsphysicaldisciplinemusicandartplatobelievedthattalentandintelligenceisnotdistributedgeneticallyandthusisbefoundinchildrenborntoallclassesalthoughhisproposedsystemofselectivepubliceducationforaneducatedminorityofthepopulationdoesnotreallyfollowademocraticmodelaristotleconsideredhumannaturehabitandreasontobeequallyimportantforcestobecultivatedineducationtheultimateaimofwhichshouldbetoproducegoodandvirtuouscitizensheproposedthatteachersleadtheirstudentssystematicallyandthatrepetitionbeusedasakeytooltodevelopgoodhabitsunlikesocratesemphasisonquestioninghislistenerstobringouttheirownideasheemphasizedthebalancingofthetheoreticalandpracticalaspectsofsubjectstaughtamongwhichheexplicitlymentionsreadingwritingmathematicsmusicphysicaleducationliteraturehistoryandawiderangeofsciencesaswellasplaywhichhealsoconsideredimportantduringthemedievalperiodtheideaofperennialismwasfirstformulatedbystthomasaquinashisinworkdemagistroperennialismholdsthatoneshouldteachthosethingsdeemedtobeofeverlastingimportancetoallpeopleeverywherenamelyprinciplesandreasoningnotjustfactswhichareapttochangeovertimeandthatoneshouldteachfirstaboutpeoplenotmachinesortechniquesitwasoriginallyreligiousinnatureanditwasonlymuchlaterthatatheoryofsecularperennialismdevelopedduringtherenaissancethefrenchskepticmicheldemontaignewasoneofthefirsttocriticallylookateducationunusuallyforhistimemontaignewaswillingtoquestiontheconventionalwisdomoftheperiodcallingintoquestionthewholeedificeoftheeducationalsystemandtheimplicitassumptionthatuniversityeducatedphilosopherswerenecessarilywiserthanuneducatedfarmworkersforexample";
+    char plainrandtext[100];
+    srand(time(0));
+
+    plainrandtext[0] = '\0'; //gpt made these lines:
+    int rand_start = rand() % (strlen(randtext) - 100); // ensure at least 100 chars available
+    int rand_len = 49 + rand() % 50; // copy 90 to 99 characters
+    strncpy(plainrandtext, randtext + rand_start, rand_len);
+    plainrandtext[rand_len] = '\0';
+
+    char cipherrandtext[100];
+    int key[100];
+    int len=makekey(plainrandtext,randtext,key);
+    encrypt(plainrandtext,cipherrandtext,key,len);
+    int num=(int)ceil(strlen(cipherrandtext)/22.0);
+    makepackets(cipherrandtext,packets_out);
+
+}
+
 int main(){
     char plaintext[100];
     char ciphertext[100];
@@ -66,17 +95,20 @@ int main(){
 
     encrypt(plaintext,ciphertext,keystream,len_of_key);
 
-    char packets[(int)ceil(strlen(ciphertext)/25.0)][26];
+    int numpacks=(int)ceil(strlen(ciphertext)/22.0);
+    char packets[numpacks][26];
+    char junk [100][26];
+    for(int i=0;i<100;i++){strcpy(junk[i],"\0");}
     makepackets(ciphertext,packets);
-
-    
+    makejunk(junk);
     printf("%s\n",ciphertext);
     decrypt(ciphertext,plaintext,keystream,len_of_key);
     printf("%s\n",plaintext);
 
     
-    for(int i=0;i<sizeof(packets)/sizeof(packets[0]);i++){printf("%s",packets[i]);}
-    printf("\n%s\n",ciphertext);
+    for(int i=0;i<sizeof(packets)/sizeof(packets[0]);i++){printf("%s ",packets[i]);}
+    printf("\n%s\n\n",ciphertext);
+    for(int i=0;i<sizeof(junk)/sizeof(junk[0]);i++){if(junk){printf("%s ",junk[i]);}}
 
     return 0;
 }
